73268aa0579e4c69f8bf0407b0e60b2e4fbcff4f
Abhilash Madi <madiabhilash1563@gmail.com>
2022-11-27 15:53:39 +0530

    Live User Filter

---
+++ 18.UserListUi/script.js
@@ -0,0 +1,43 @@
+const result = document.getElementById("result") //ul
+const filter = document.getElementById("filter") //input
+const listItems = [];
+const users = 150;
+
+//GET Data from the randomuser.me api service...
+async function fetchData(){
+    const response = await fetch(`https://randomuser.me/api?results=${users}`);
+    
+    //destructuring the api given data and taking results from it
+    const { results } = await response.json();
+
+    result.innerHTML = "";
+
+    results.forEach((user) => {
+        const li = document.createElement("li");
+        listItems.push(li);
+        li.innerHTML = `
+        <img src="${user.picture.large}" alt="${user.name.first} ${user.name.last}" class="user-image">
+        <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p><span>${user.location.state}</span>,<span class="country">${user.location.country}</span></p>
+        </div>
+        `;
+        
+        result.appendChild(li);
+    })
+}
+fetchData();
+
+//funciton to get Input for filtering the users
+
+function filterData(searchTerm){
+    listItems.forEach((item) => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove("hide");
+        } else {
+            item.classList.add("hide")
+        }
+    })
+}
+
+filter.addEventListener("input",(e) => filterData(e.target.value));
